26d6d702011cae5480248acbe8cd6e889cd29919
akami11 <cerojp24@gmail.com>
2021-04-05 05:26:24 +0000

    [Finish]header

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,35 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.save
+    redirect_to categories_path
   end
 
   def edit
+    @categories = Category.all
+    @category = Category.find(params[:id])
   end
 
   def update
+    @category = Category.find(params[:id])
+    @category.update(category_params)
+    redirect_to categories_path
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name)
   end
 end
